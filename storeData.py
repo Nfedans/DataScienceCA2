83e94a8f0fc443ee195fd57ed42f85858eafe576
Nfedans <nfedans@gmail.com>
2022-10-30 18:26:17 +0000

    Scrape all useful information for all videos available

--- storeData.py
+++ storeData.py
@@ -1,10 +1,3 @@
-# This project will scrape data from a video streaming website called Rumble
-# Rumble's robots.text file as of 14:42 on the 30th October 2022 says:
-# User-agent: *
-# Disallow: /l/
-# Disallow: /api/
-# Therefore, scraping video/channel lists is perfectly legal
-
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
@@ -15,25 +8,55 @@ urlStringNumberedPage = 'https://rumble.com/videos?sort=views&date=this-month&pa
 
 video_details = []
 counter = 0
+number_of_pages = 100
 
-for page in range(1, 2):
+
+def appendPaymentTransparentVideo():
+    video_details.append([
+        str(item('h3')[0].contents[0])
+        , str(item('div')[0].contents[0])
+        , str(item('span')[0]['data-value'])
+        , str(item('span')[1]['data-value'])
+        , str(item('span')[2]['data-value'])
+        , str(item('span')[3]['data-value'])
+        , str(item('time')[0]['datetime'])
+    ])
+
+
+def appendVideo():
+    video_details.append([
+        str(item('h3')[0].contents[0])
+        , str(item('div')[0].contents[0])
+        , str(item('span')[0]['data-value'])
+        , 'NA'
+        , str(item('span')[1]['data-value'])
+        , str(item('span')[2]['data-value'])
+        , str(item('time')[0]['datetime'])
+    ])
+
+
+for page in range(1, (number_of_pages + 1)):
     if (page == 1):
         urlString = urlStringFirstPage
     else:
         urlString = urlStringNumberedPage + str(page)
+
     html = requests.get(urlString).text
     soup = BeautifulSoup(html, 'html.parser')
 
     for item in soup('article'):
         try:
             if (item['class'][0] == "video-item"):
-                video_details.append(str(item('h3')[0].contents[0]))
-
+                # print(item('span')[1]['class'][1])
+                if (item('span')[1]['class'][1] == 'video-item--earned'):
+                    appendPaymentTransparentVideo()
+                else:
+                    appendVideo()
                 counter += 1
         except:
             pass
         time.sleep(1)
 
-videopd = pd.DataFrame(video_details, columns=['Title'])
+videopd = pd.DataFrame(video_details, columns=['Title', 'Channel', 'Duration', 'Earned', 'Views', 'Likes', 'Uploaded'])
 
 
