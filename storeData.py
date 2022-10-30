dc462d5b2de579a0190364cd5e7f16ec68823d7c
Nfedans <nfedans@gmail.com>
2022-10-30 14:47:22 +0000

    Add storeData file and robots.txt evidence

---
+++ storeData.py
@@ -0,0 +1,7 @@
+# This project will scrape data from a video streaming website called Rumble
+# Rumble's robots.text file as of 14:42 on the 30th October 2022 says:
+# User-agent: *
+# Disallow: /l/
+# Disallow: /api/
+# Therefore, scraping video/channel lists is perfectly legal
+
