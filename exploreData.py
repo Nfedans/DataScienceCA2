ad1ac460232d3897280224cd7600f4c287a5e1d9
Nfedans <nfedans@gmail.com>
2022-11-04 01:02:37 +0000

    almost done it all

--- exploreData.py
+++ exploreData.py
@@ -1,27 +1,25 @@
 # Data gives details of all videos which made it to the top 50 pages of the month on rumble.com
 # Variables: index, title, channel, duration, $ earned, views, Likes, upload date, and channel subscriber count.
 
-#Response (Dependent) variable is "index"
+#Response (Dependent) variable is "Rank"
 
-#We are interested in the key variables of Duration, Views, Likes, upload date, Subscribers
+#We are interested in the key variables of Duration, Views, Likes, Rank, Subscribers
 
 # STEP 1 : BUSINESS UNDERSTANDING
 #
 # I am trying to predict based on the variables on line 6, and perhaps variables added in the future
 # as part of feature engineering, how a video might rank in the top 50 pages of videos, on Rumble
 #
-# If I can develop a sufficiently accurate model, and find how variables correlate, this will be 
+# If I can develop a sufficiently accurate model, and find how variables correlate, this will be
 # useful information for companies that want to advertise their products through rumble creators.
-# 
+#
 # Some questions i intend to answer:
 # 1) What channels consistently make it to the top 50
 # 2) is there a relationship between the amount of likes on the video and amount of subscribers of the publishing channel?
 # 3) What does the distribution of videos that make it to the top 50 by video duration look like?
-# 4) Do longer videos get more views than short videos?
+# 4) Is there correlation between channel subscribers and video likes?
 # 5) is there a relationship between the views and the amount of subscribers of the publishing channel?
-# 
-# THINK OF SOME BETTER STUFF WHEN I GET TO DOING IT
-# 
+#
 # Who Benefits?
 # 1) Companies looking for advertisement deals with content creators(They will see what they should pay attention to when evaluating a channel
 # , subscriber count? video length? video upload frequency? how many likes the videos get?... or some combination of the variables listed.)
@@ -44,8 +42,6 @@ import matplotlib.pyplot as plt
 #To set figure size
 from matplotlib.pyplot import figure
 
-# For colorful output
-from colorama import Fore, Style
 import re
 import matplotlib.ticker as mtick
 from matplotlib.ticker import FuncFormatter
@@ -119,7 +115,7 @@ data.drop('Earned', axis = 1, inplace = True)
 # Drop redundant index
 data.drop('Unnamed: 0', axis = 1, inplace = True)
 
-# Make DURATION into a manipulatible format
+# Make DURATION into a manipulatible format (seconds)
 data['Duration Seconds'] = data['Duration'].apply(lambda x: get_sec(x))
 
 # # Delete all videos with duration LIVE (-1 in Duration Seconds Column)
@@ -136,18 +132,15 @@ data['Views'] = data['Views'].apply(lambda x: int(x.translate({ord(','): None}))
 data['Likes'] = data['Likes'].apply(lambda like_count: format_numbers(like_count))
 data['Subscribers'] = data['Subscribers'].apply(lambda subs: format_numbers(subs))
 
-
-
 # Check / Make UPLOADED a manipulatable format
 
-# MAYBE DO THIS DURING FEATURE ENGINEERING
 data.info()
 
 
 #3. MISSING VALUES ######################################################
 
 
-# Checking the missing values (Trust me, they are all there...)
+# Checking the missing values
 numberMissing=data.isnull().sum()
 
 
@@ -175,14 +168,15 @@ plt.show()
 
 data.Likes.describe()
 
-# there certainly are outliers, however after checking out the top 3 cases
+# there certainly are outliers, however after checking out the top 2 cases
 # they seem reasonable, the top most liked video was a giveaway contest
 # the second most liked was made by a scam artist with a big cult following
 # I end up removing the top most liked video though, as there was a big incentive to
 # watch it and participate in the giveaway, so people who generally do not use
 # Rumble may have heard about it and watched and liked just to participate
 
-data = data.drop(data[data.Likes > 50000].index)
+# The top result is  74.6k likes
+data = data.drop(data[data.Likes == 74600].index)
 
 figure(num=None, figsize=(8, 6), dpi=80, facecolor='w', edgecolor='k')
 plt.boxplot(x=data.Likes)
@@ -207,19 +201,23 @@ print(channel_details)
 number_channel_vids = data.Channel.value_counts()
 
 fig, ax = plt.subplots(figsize=(3, 4.5),  dpi=163)
+# there are 233 channels that have their videos in the top 50
 ax.set_ylim([0, 233])
+# this is for the percentage ticks on the y axis
 rng = np.arange(0, 240, 11.65)
 ax.set_yticks(rng)
 ax.hist(number_channel_vids, bins=range(0, 90, 5))
-ax.set_title("Histogram of Channels")
+ax.set_title("")
 ax.set_ylabel("amount of channels")
 ax.set_xlabel("amount of videos")
 
+# Secondary axis, this one will be on the right, number format
 secax = ax.twinx()
 secax.hist(number_channel_vids, bins=range(0, 90, 5))
 secax.set_ylim([0, 233])
 secrng = np.arange(0, 240, 10)
 secax.set_yticks(secrng)
+# Format ax y axis as pecentage values
 ax.yaxis.set_major_formatter(mtick.PercentFormatter(xmax=len(number_channel_vids)))
 plt.show()
 
@@ -230,30 +228,6 @@ plt.show()
 # of your videos to make it to the top 50.
 
 
-
-
-
-# # This calculates for each channel that made it to the top 50, how many of that channel's videos
-# # are in the top 50. A histogram is used to represent this data, because a bar chart or similar
-# # would be too big (233 channels), therefore splitting into bins, and showing that way is better.
-# like_details = data.Likes.unique()
-# print(like_details)
-
-# number_likes = data.Likes.value_counts()
-
-# fig, ax = plt.subplots(figsize=(15, 10),  dpi=163)
-# #ax.set_xlim([0, 45])
-# rng = np.arange(0, 44, 1)
-# #ax.set_xticks(rng)
-# #ax.set_ylim([0, 45])
-# ax.hist(number_likes)
-# ax.set_title("Histogram of Likes")
-# ax.set_ylabel("1000's of likes")
-# ax.set_xlabel("amount of videos with the likes")
-# plt.show()
-
-# figure(num=None, figsize=(12, 100), dpi=80, facecolor='w', edgecolor='k')
-# number_likes.plot.barh()
 
 figure(num=None, figsize=(10, 10), dpi=80, facecolor='w', edgecolor='k')
 plt.scatter(data.index, data['Likes'])
@@ -298,10 +272,8 @@ data.Views.describe()
 
 
 # SUMMARY STATEMENT:
-# The average amount of views in the top 50 pages worth of videos is just under 120,000, If you want a chance to land
+# The average (mean) amount of views in the top 50 pages worth of videos is just over 60,000, If you want a chance to land
 # in the top 50 pages, your video needs over 24,000 views minimum. The most viewed video has close to 2.5 million views.
-# subscriptions_pegged_to_channels = data.Subscribers.value_counts()
-# df = pd.DataFrame (subscriptions_pegged_to_channels, columns = ['Subs'])
 
 unique_subs = data.Subscribers.unique()
 subdf = pd.DataFrame(unique_subs, columns = ['Subs'])
@@ -335,60 +307,36 @@ subdf.median()
 
 
 
-
-# number_channel_vids = data.Channel.value_counts()
-
-# fig, ax = plt.subplots(figsize=(3, 4.5),  dpi=163)
-# ax.set_ylim([0, 233])
-# rng = np.arange(0, 240, 11.65)
-# ax.set_yticks(rng)
-# ax.hist(number_channel_vids, bins=range(0, 90, 5))
-# ax.set_title("Histogram of Channels")
-# ax.set_ylabel("amount of channels")
-# ax.set_xlabel("amount of videos")
-
-# secax = ax.twinx()
-# secax.hist(number_channel_vids, bins=range(0, 90, 5))
-# secax.set_ylim([0, 233])
-# secrng = np.arange(0, 240, 10)
-# secax.set_yticks(secrng)
-# ax.yaxis.set_major_formatter(mtick.PercentFormatter(xmax=len(number_channel_vids)))
-# plt.show()
-
 fig, ax = plt.subplots(figsize=(12, 8),  dpi=163)
-ax.hist(data['Duration Seconds'], bins=range(0, 54000, 300))
+ax.hist(data['Duration Seconds'], bins=range(0, 54400, 300))
 ax.set_ylim([0, 430])
-# ax.set_xlim([0, 12])
 ax.set_title("Histogram of Video lengths")
 ax.set_ylabel("percentage of all videos")
 ax.set_xlabel("hours (each bar = 5 minutes)")
 
+# This is a secondary y axis
 secax = ax.twinx()
-secax.hist(data['Duration Seconds'], bins=range(0, 54000, 300))
+# 300 seconds = 5 minutes
+secax.hist(data['Duration Seconds'], bins=range(0, 54400, 300))
 secax.set_ylim([0, 430])
 secax.set_ylabel("Amount of videos")
 
-
+# format y axis as percentages, going up in 2.5% inervals
 ax.yaxis.set_major_formatter(mtick.PercentFormatter(xmax=len(data['Duration Seconds'])))
 rng = np.arange(0, 430, 30.525)
 ax.set_yticks(rng)
-xrng = np.arange(0, 50000, 3600)
+xrng = np.arange(0, 54400, 3600)
 ax.set_xticks(xrng)
 
-
+# this function is used to convert the x axis from seconds to hours (3600 becomes 1, 7200 becomes 2...)
 def numfmt(x, pos):
-    s = '{}'.format(x / 3600).rstrip('.0')
-    # if s > 0:
-    #     return int(s)
-    # else:
-    #     return "0"
+    s = '{}'.format(x / 3600)
+    size = len(s)
+    s = s[:size - 2]
     return s
 
 yfmt = FuncFormatter(numfmt)
-
 ax.xaxis.set_major_formatter(yfmt)
-#ax.savefig("Output.pdf", dpi=400, bbox_inches='tight', pad_inches=1)
-
 plt.show()
 
 
@@ -406,4 +354,137 @@ data['Duration Seconds'].describe()
 
 # SUMMARY STATEMENT:
 # 1/3 of videos in the top 50 ranked pages are 5 minutes long or less, 10% of videos are between 5 and 10 minutes, 7% of videos are
-# between 55 minutes and an hour.
+# between 55 minutes and an hour. It is interesting that 55 - 60 minutes is the next most popular video upload length after 5 - 10 minute
+# videos and sub 5 minute videos, but may hypothetically be attributed to creators having an hour free in their timetable and making use of it.
+
+
+
+#6. EXPLORATORY DATA ANALYSIS - BIVARIATE ANALYSIS ############################################
+
+####Numeric - numeric analysis for two variables where both are numeric
+
+
+#plot the scatter plot of views and likes variable in data
+figure(num=None, figsize=(8, 8), dpi=163, facecolor='w', edgecolor='k')
+plt.scatter(data.Views,data.Likes)
+plt.title("Views vs Likes")
+plt.xlabel("Views", size=20)
+plt.ylabel("Likes", size=20)
+plt.show()
+
+#Calculate correlation
+np.corrcoef(data.Views, data.Likes)
+data[['Views','Likes']].corr()
+
+#Summary Statement - There is a weak correlation of 0.334378 between Views And Likes
+
+
+
+#plot the scatter plot of subscribers and Views variable in data
+figure(num=None, figsize=(8, 8), dpi=163, facecolor='w', edgecolor='k')
+plt.scatter(data.Subscribers,data.Views)
+plt.title("Subscribers vs Views")
+plt.xlabel("Subscribers", size=20)
+plt.ylabel("Views", size=20)
+plt.show()
+
+#Calculate correlation
+np.corrcoef(data.Subscribers, data.Views)
+data[['Subscribers','Views']].corr()
+
+#Summary Statement - There is a very weak correlation of 0.080955 between Subscribers And Views
+
+
+
+#plot the scatter plot of subscribers and Likes variable in data
+figure(num=None, figsize=(8, 8), dpi=163, facecolor='w', edgecolor='k')
+plt.scatter(data.Subscribers,data.Likes)
+plt.title("Subscribers vs Likes")
+plt.xlabel("Subscribers", size=20)
+plt.ylabel("Likes", size=20)
+plt.show()
+
+#Calculate correlation
+np.corrcoef(data.Subscribers, data.Likes)
+data[['Subscribers','Likes']].corr()
+
+#Summary Statement - There is moderate correlation of 0.530824 between Subscribers and Likes
+
+
+
+####Correlation Matrix and Heat Map
+
+# Creating a matrix
+data[['Subscribers','Rank','Likes','Views','Duration Seconds']].corr()
+
+#plot the correlation matrix
+sns.heatmap(data[['Subscribers','Rank','Likes','Views','Duration Seconds']].corr(), annot=True, cmap = 'Reds')
+plt.show()
+
+
+#Summary Statement - Correlation strength, strongest to weakest:
+#
+#   1) View / Rank = -0.58
+#   2) Likes/ Subscribers = 0.53
+#   3) Likes/ Rank = -0.41
+#   4) Like / Views = 0.33
+#   5) Duration Seconds / Likes = 0.28
+#   6) Rank / Subscribers = -0.17
+#   7) Duration Seconds / Views = 0.11
+#   8) Views / Subscribers = 0.081
+#   9) Duration seconds / Rank = -0.059
+#   10) Duration seconds / Subscribers = -0.012
+#
+
+
+#####Numeric - Categorical Analysis
+#Analyzing the one numeric variable and one categorical variable from a dataset is known as numeric-categorical analysis.
+#We analyze them mainly using mean, median, and box plots.
+#Investigate salary and response columns from our dataset.
+
+figure(num=None, figsize=(32, 32), dpi=160, facecolor='w', edgecolor='k')
+mean_likes_per_channel=data.groupby('Channel')['Likes'].mean()
+data.groupby('Channel')['Likes'].median()
+
+mean_likes_per_channel.plot.barh(figsize=(40,40))
+plt.title("Likes by Channel")
+plt.show()
+
+
+figure(num=None, figsize=(30, 30), dpi=80, facecolor='w', edgecolor='k')
+sns.boxplot(x=data.Likes, y=data.Channel)
+plt.tight_layout()
+plt.show()
+
+# While some channels may have highly varying amounts of likes for their videos, generally
+# Speaking there are not many outliers per channel, unlike when looking at a boxplot of likes overall
+# Which shows quite a lot of outliers, this means the same channels get large amounts of likes all the time
+# Making them seem as "Outlier creators."
+
+
+#7. EXPLORATORY DATA ANALYSIS - Multivariate ANALYSIS ############################################
+
+
+#If we analyze data by taking more than two variables/columns into consideration from a dataset, it is known as Multivariate Analysis.
+#Let’s investigate how ‘Education’, ‘Marital’, and ‘Response_rate’ vary with each other.
+#First, we’ll create a pivot table with the three columns and after that, we’ll create a heatmap.
+
+#Pivot table automatically uses the mean value of the response_rate argument.
+result = pd.pivot_table(data=data, index='Views', columns='Likes',values='Subscribers')
+print(result)
+
+#create heat map of Views vs Likes vs Subscribers
+figure(num=None, figsize=(8, 8), dpi=80, facecolor='w', edgecolor='k')
+sns.heatmap(result, annot=True, cmap = 'RdYlGn', center=0.117)
+plt.show()
+
+#Based on the Heatmap we can infer that the married people with primary education are less likely to respond positively for the survey and single people with tertiary education are most likely to respond positively to the survey.
+#Similarly, we can plot the graphs for Job vs marital vs response, Education vs poutcome vs response, etc.
+
+
+
+
+
+
+
+
